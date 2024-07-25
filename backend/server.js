e7c113e3dc58e0062dc1d66a7c52d3e53221eef9
Arpit <arpitvermaetw@gmail.com>
2024-07-25 14:39:37 +0530

    Add SOme

--- backend/server.js
+++ backend/server.js
@@ -30,16 +30,20 @@ const server = app.listen(PORT, () => {
 const io = new Server(server, {
   pingTimeout: 60000,
   cors: {
-    origin: process.env.SOCKETORIGIN
+    origin: "http://localhost:5173",
+  
   },
 });
 
-// (server , {
-//   pingTimeout : 60000,
-//   cors : {
-//     origin : "http://localhost:3000",
-//   }
-// })
+// const cors = require('cors');
+
+// const corsOptions = {
+//   origin: 'http://localhost:5173',
+//   methods: ['GET', 'POST'],
+//   allowedHeaders: ['Content-Type', 'Authorization'],
+// };
+
+// app.use(cors(corsOptions));
 
 io.on("connection", (socket) => {
   console.log("Connected to socket.io");
