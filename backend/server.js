e3c6e07094736e2e31f507a77448cc0e4e53d083
Arpit <arpitvermaetw@gmail.com>
2024-08-12 14:16:45 +0530

    Change Origin

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,7 @@ const PORT = process.env.PORT || 4000;
 // const io = require("socket.io")
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173" ,
+    origin: "https://chat-unity.netlify.app" ,
     credentials: true,
   },
 });
